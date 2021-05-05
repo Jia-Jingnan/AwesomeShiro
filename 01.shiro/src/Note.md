3b01ced9df7ddc72acc2c2ad4e928fbe910bcfd9
Jia-Jingnan <875480307@qq.com>
2021-05-05 22:25:50 +0800

    0505-Shiro中md5加密，加盐，加散列的使用

--- 01.shiro/src/Note.md
+++ 01.shiro/src/Note.md
@@ -5,4 +5,9 @@
 AuthenticatingRealm 认证realm doGetAuthenticationInfo  
 AuthorizingRealm 授权realm doGetAuthorizationInfo  
 AuthorizingRealm继承了AuthenticatingRealm，具有doGetAuthenticationInfo（认证） 和 doGetAuthorizationInfo（授权）   
-SimpleAccountRealm就是继承了AuthorizingRealm,同时拥有认证和授权两个方法
+SimpleAccountRealm就是继承了AuthorizingRealm,同时拥有认证和授权两个方法  
+
+MD5加密：  
+作用：一般用来加密或签名（校验和）  
+特点：MD5算法不可逆，如果内容相同，无论执行多少次md5生成的结果始终都是一致的  
+生成结果：始终是一个16进制32位长度字符串
---
+++ 01.shiro/src/main/java/com/lilith/md5/ShiroMD5.java
@@ -0,0 +1,32 @@
+package com.lilith.md5;
+
+import org.apache.shiro.crypto.hash.Md2Hash;
+import org.apache.shiro.crypto.hash.Md5Hash;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 下午10:14 2021/5/5
+ */
+public class ShiroMD5 {
+
+    public static void main(String[] args) {
+
+        Md5Hash md5Hash = new Md5Hash();
+        md5Hash.setBytes("123".getBytes());
+
+        String s = md5Hash.toHex();
+        System.out.println(s);
+
+        // 使用md5加密
+        Md5Hash md5Hash1 = new Md5Hash("123");
+        System.out.println(md5Hash1.toHex());
+
+        // md5 + salt
+        Md5Hash md5Hash2 = new Md5Hash("123", "Xfewad*");
+        System.out.println(md5Hash2.toHex());
+
+        // md5 + salt + hash散列
+        Md5Hash md5Hash3 = new Md5Hash("123", "Xfewad*", 1024);
+        System.out.println(md5Hash3);
+    }
+}
